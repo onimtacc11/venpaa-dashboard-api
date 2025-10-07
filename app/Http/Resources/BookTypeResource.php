5b9da0aacba6288ac3233e45944bbd479912297b
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-07 09:56:11 +0530

    Modified

--- app/Http/Resources/BookTypeResource.php
+++ app/Http/Resources/BookTypeResource.php
@@ -15,8 +15,8 @@ class BookTypeResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'bkt_code'     => $this->loca_code,
-            'bkt_name'     => $this->loca_name,
+            'bkt_code'     => $this->bkt_code,
+            'bkt_name'     => $this->bkt_name,
             'created_by'    => $this->created_by,
             'updated_by'    => $this->updated_by,
         ];
