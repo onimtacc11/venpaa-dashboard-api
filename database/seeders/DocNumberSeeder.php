14337af3e7d60d3c6561502e7f375ab9d74bc79d
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-10 16:40:44 +0530

    Add & handle publisher doc number

--- database/seeders/DocNumberSeeder.php
+++ database/seeders/DocNumberSeeder.php
@@ -22,6 +22,7 @@ class DocNumberSeeder extends Seeder
             ['type' => 'Department', 'prefix' => 'DEP', 'last_id' => 0],
             ['type' => 'SubCategory', 'prefix' => 'SC', 'last_id' => 0],
             ['type' => 'Category', 'prefix' => 'CAT', 'last_id' => 0],
+            ['type' => 'Publisher', 'prefix' => 'PUB', 'last_id' => 0],
         ];
 
         foreach ($default as $key => $value) {
